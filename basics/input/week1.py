df69f81192bdd9d232ce461546d788775dc85071
repl.it user <replituser@example.com>
2020-10-06 16:20:47 +0000

    upto input task in week1

---
+++ basics/input/week1.py
@@ -0,0 +1,23 @@
+print("Please enter a character for the eye")
+eye=input()
+print("Beep's expression is now as follows:")
+print("##########")
+print("#  "+eye+"  "+eye+"  #")
+print("#  ----  #")
+print("##########")
+
+
+
+print(10+10)
+
+
+print("What is Your Name?")
+name=input()
+print("What is Your Age")
+age=int(input())
+print("What is Your Height?")
+height=float(input())
+print("What is Your Weight")
+weight=float(input())
+bmi=weight/(height*height)
+print(name,"you are",age,"years old and your bmi is",bmi)
---
+++ main.py
@@ -0,0 +1,24 @@
+print("Hello")
+print("Hello World")
+# Display message to the standard output
+print("System Failure Imminent")
+print("System reboot has been initiated...")
+print()
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print()
+print("System online.")
+
+# Alternative 
+print(""""
+System Failure Imminent!
+System reboot has been initiated...
+
+...rebooting sensory system
+...rebooting output motors
+...rebooting hover engine
+
+System online.
+""")
+print("Hello My Name Is \"Titus\" \n\t How Are You")
