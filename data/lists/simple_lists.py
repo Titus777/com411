5cfd88a28feb141687008bbbc2ba092495e12a4f
repl.it user <replituser@example.com>
2020-10-26 13:20:59 +0000

    populate_lists

--- data/lists/simple_lists.py
+++ data/lists/simple_lists.py
@@ -1,8 +1,10 @@
 def directions():
-  directions = ["Move Foward", "Move Backkward", "Turn Left", "Turn Right"]
-  return directions
+    directions = ["Move Foward", "Move Backkward", "Turn Left", "Turn Right"]
+    return directions
+
 
 def run():
-  print(directions())
+    print(directions())
+
 
 run()
